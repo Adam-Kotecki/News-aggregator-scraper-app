ed49162fc26db038c424dcf52e92ce00e2682530
Adam-Kotecki <a.kotecki11@gmail.com>
2022-12-18 13:18:36 +0100

    second website to choose from added

--- News scraper app.py
+++ News scraper app.py
@@ -2,47 +2,87 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
+web = input("Please provide website (Lobsters or Hackers): ")
 subject = input("Please provide keyword of subject:")
 recent = int(input("How many recent articles to search:"))
 provided_points = int(input("Please provide minimal number of points"))
 
-res = requests.get('https://lobste.rs/')
+if web == "Hackers":
+    website = "https://news.ycombinator.com"
+    class1 = '.titleline > a'
+    class2 = '.subtext'
+if web == "Lobsters":
+    website = "https://lobste.rs/"
+    class1 = '.u-url'
+    class2 = '.score'
+
+res = requests.get(website)
 soup = BeautifulSoup(res.text, 'html.parser')
-links = soup.select('.u-url')
-scores = soup.select('.score')
+links = soup.select(class1)
+score = soup.select(class2)
 
 stop = 1
 
-if recent > 25:
-    if recent == 50:
-        stop = 2
-    if recent == 75:
-        stop = 3
-    if recent == 100:
-        stop = 4
-    if recent == 125:
-        stop = 5
+if web == "Hackers":
+    if recent > 30:
+        if recent == 60:
+            stop = 2
+        if recent == 90:
+            stop = 3
+        if recent == 120:
+            stop = 4
+        if recent == 150:
+            stop = 5
+if web == "Lobsters":
+    if recent > 25:
+        if recent == 50:
+            stop = 2
+        if recent == 75:
+            stop = 3
+        if recent == 100:
+            stop = 4
+        if recent == 125:
+            stop = 5
+        
+if web == "Hackers":
+    web_page = 'https://news.ycombinator.com/?p='
+if web == "Lobsters":
+    web_page = 'https://lobste.rs/page/'
+
     
     for page in range(2, stop + 1):
-        res = requests.get('https://lobste.rs/page/' + str(page))
+        res = requests.get(web_page + str(page))
         if res.status_code == 200:
             soup2 = BeautifulSoup(res.text, 'html.parser')
             # Extracting only needed data:
-            links2 = soup2.select('.u-url')
-            scores2 = soup2.select('.score')
+            links2 = soup2.select(class1)
+            score2 = soup2.select(class2)
             links = links + links2
-            scores = scores + scores2
+            score = score + score2
 
-def interesting_content(links, scores):
+def interesting_content(links, score):
     content = []
-    for index, item in enumerate(links):
-        title = item.getText()
-        href = item.get('href', None)
-        points = scores[index].getText()
-        points_no = int(points)
-        # selecting only ones which match criteria:
-        if (points_no >= provided_points) and (subject.lower() in title.lower()):
-            content.append({'title': title, 'link': href, 'votes': points_no})
+    if web == "Hackers":
+        for index, item in enumerate(links):
+            title = item.getText()
+            href = item.get('href', None)
+            points = score[index].select('.score')
+            # extracting only articles with points:
+            if len(points) > 0:
+                points_no = int(points[0].getText().replace(' points', ''))
+                # selecting only ones which match criteria:
+            if (points_no >= provided_points) and (subject.lower() in title.lower()):
+                content.append({'title': title, 'link': href, 'votes': points_no})
+    if web == "Lobsters":
+        for index, item in enumerate(links):
+            title = item.getText()
+            href = item.get('href', None)
+            points = score[index].getText()
+            # extracting only articles with points:
+            #if len(points) > 0:
+            points_no = int(points)
+            if (points_no >= provided_points) and (subject.lower() in title.lower()):
+                    content.append({'title': title, 'link': href, 'votes': points_no})
     write_content(content)
     return content
 
@@ -54,4 +94,4 @@ def write_content(content):
             f.write(str(ind) + ". " + str(dict['title']) + " |votes: " + str(dict['votes']) + "|  " + str(dict['link']) + '\n' )
             ind = ind + 1
 
-interesting_content(links, scores)
+interesting_content(links, score)
